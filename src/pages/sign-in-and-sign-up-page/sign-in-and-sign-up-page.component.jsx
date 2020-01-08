8af58ef61f401f95c17c28862330fe2395f3d7ef
Akshat Jain <akshatjain0027@gmail.com>
2020-01-08 16:20:07 +0530

    just made few changes in heading styles

--- src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component.jsx
+++ src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import SignIn from '../../components/sign-in/sign-in.component'
 import SignUp from '../../components/sign-up/sign-up.component'
+import './sign-in-and-sign-up-page.styles.css'
 
 const SignInAndSignUpPage = () => (
     <div className='SignIn-SignUp'>
